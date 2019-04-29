d3eb5eafbf7c4ae5f4674d147e7f0931808164fa
Jacob DeLeon <jacobdeleon12@gmail.com>
2019-04-29 11:56:22 -0700

    added read file
    added read file

--- liri.js
+++ liri.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 var fs = require("fs");
 var keys = require("./keys.js");
 var axios = require("axios");
-// var inquirer = require("inquirer");
+// var inquirer = require("inquirer"); I want to add this in later
 var moment = require("moment");
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
@@ -15,8 +15,10 @@ var spotify = new Spotify(keys.spotify);
 
 // function for imdb
 var userMovie = function (movieName) {
-
-    var movieName = process.argv.slice(3).join(" ");
+    if (movieName === undefined) {
+        movieName = "speed";
+    }
+    
     var url = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
     axios.get(url).then(function (response) {
@@ -45,6 +47,9 @@ var userMovie = function (movieName) {
 
 
 var userBand = function (artist) {
+    if (artist === undefined) {
+        artist = "tool";
+    }
 
     var artist = process.argv.slice(3).join(" ");
     var url = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
@@ -63,53 +68,81 @@ var userBand = function (artist) {
             console.log(data.venue.country);
             //      * Date of the Event (use moment to format this as "MM/DD/YYYY")
             console.log(moment(data.datetime).format("MM/DD/YYYY"));
-            console.log("\r\n");
+            console.log("\n-----------------------------------\n");
+            
         }
     });
 }
 
-var userSong = function(song){
-    
-    var song = process.argv.slice(3).join(" ");
+var userSong = function (song) {
 
-    spotify.search({ type: 'track', query: song }, function(err, data) {
-      if (err) {
-        return console.log('Error occurred: ' + err);
-      }
-     
-    //   console.log(data.tracks); 
-      console.log(data.tracks.items); 
+    // * If no song is provided then your program will default to "The Sign" by Ace of Base.
+    if (song === undefined) {
+        song = "The Sign";
+    }
+
+    spotify.search({ type: 'track', query: song }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        //   console.log(data.tracks); 
+        songs = data.tracks.items;
+
+        for (var i = 0; i < songs.length; i++) {
+            console.log(i);
+            // * Artist(s)
+            console.log("artist(s): " + songs[i].artists.name)
+            // * The song's name
+            console.log("The song's name: " + songs[i].name);
+            // * A preview link of the song from Spotify
+            console.log("A preview link of the song from Spotify: " + songs[i].preview_url);
+            // * The album that the song is from
+            console.log("The album that the song is from: " + songs[i].album.name);
+            console.log("\n-----------------------------------\n");
+        }
     });
 }
 
-// * This will show the following information about the song in your terminal/bash window
+var doWhatItSays = function () {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        console.log(data);
+        var dataArr = data.split(",");
 
-// * Artist(s)
+        if (error) {
+            return console.log('Error occurred: ' + error);
+        }
 
-// * The song's name
+        else if (dataArr.length === 2) {
+            chooseArg(dataArr[0], dataArr[1]);
+        } 
+        else if(dataArr.length === 1) {
+            chooseArg(dataArr[0]);
+        }
 
-// * A preview link of the song from Spotify
+    });
+};
 
-// * The album that the song is from
-
-// * If no song is provided then your program will default to "The Sign" by Ace of Base.
-
-
-
-
-
-var runApp = function () {
-    if (process.argv[2] === "movie-this") {
-        userMovie();
-    } else if (process.argv[2] === "concert-this") {
-        userBand();
-    } else if (process.argv[2] === "spotify-this-song") {
-        userSong();
-    }else if (process.argv[2] === "do-what-it-says"){
-
-    }else{
+var chooseArg = function(arg1, arg2){
+    switch (arg1) {
+        case "concert-this":
+        userBand(arg2);
+          break;
+        case "spotify-this-song":
+        userSong(arg2);
+          break;
+        case "movie-this":
+        userMovie(arg2);
+          break;
+        case "do-what-it-says":
+        doWhatItSays();
+          break;
+        default:
         console.log("liri does not know what to do. Try again");
-        
-    }
+        }
+      };
+
+var runApp = function (argOne,arg2) {
+    chooseArg(argOne,arg2);
 }
-runApp();
+runApp(process.argv[2], process.argv.slice(3).join(" "));
